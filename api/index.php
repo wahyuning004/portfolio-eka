48e77915641b7277f576d8b2e833ed2d2a39ba93
wahyuning004 <wahyuning.004@gmail.com>
2026-08-20 11:15:05 +0700

    Enable debug mode and view cache path for Vercel

--- api/index.php
+++ api/index.php
@@ -25,9 +25,13 @@ foreach ($tmpDirectories as $dir) {
 $envVars = [
     'APP_ENV' => 'production',
     'APP_KEY' => 'base64:6kYV90vXwYy6zG1E8a4xQ2uN9mP8kL7jH5gF3dS1aA0=',
-    'APP_DEBUG' => 'false',
+    'APP_DEBUG' => 'true',
     'DB_CONNECTION' => 'sqlite',
     'DB_DATABASE' => ':memory:',
+    'VIEW_COMPILED_PATH' => '/tmp/storage/framework/views',
+    'SESSION_DRIVER' => 'cookie',
+    'CACHE_STORE' => 'array',
+    'LOG_CHANNEL' => 'errorlog',
 ];
 
 foreach ($envVars as $key => $val) {
